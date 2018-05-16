54dd958b148bf38f61dc8e5738830c8e4a9710c1
wanglofe <562008105@qq.com>
2018-05-17 00:31:50 +0800

    Add test function.

--- queue_circle_static_array.c
+++ queue_circle_static_array.c
@@ -11,7 +11,7 @@ typedef int queue_t;
 
 static int front = 0;
 static int rear = 0;
-static size_t m_length_length = 0;
+static size_t m_queue_length = 0;
 static queue_t queue[QUEUE_SIZE];
 
 // handler declarations
@@ -20,7 +20,7 @@ int	queue_delete(void);
 queue_t	queue_first(void);
 int	queue_isempty(void);
 int	queue_isfull(void);
-size_t	length_length(void);
+size_t	queue_length(void);
 int	queue_destroy(void);
 void	queue_print(void);
 
@@ -35,7 +35,7 @@ int queue_insert(queue_t val)
 		rear = 0;
 	}
 	queue[rear++] = val;
-	m_length_length++;
+	m_queue_length++;
 	return 0;
 }
 
@@ -49,7 +49,7 @@ int queue_delete(void)
 		front = 0;
 	}
 	front++;
-	m_length_length--;
+	m_queue_length--;
 	return 0;
 }
 
@@ -63,22 +63,22 @@ int queue_first(void)
 
 int queue_isempty(void)
 {
-	return m_length_length == 0;
+	return m_queue_length == 0;
 }
 
 int queue_isfull(void)
 {
-	return m_length_length == QUEUE_SIZE;
+	return m_queue_length == QUEUE_SIZE;
 }
 
-size_t length_length(void)
+size_t queue_length(void)
 {
-	return m_length_length;
+	return m_queue_length;
 }
 
 int queue_destroy(void)
 {
-	m_length_length = 0;
+	m_queue_length = 0;
 	front = 0;
 	rear = 0;
 	return 0;
@@ -88,7 +88,7 @@ void queue_print(void)
 {
 	int i;
 	int tmp_front = front/*, tmp_rear = rear*/;
-	for(i=0; i<m_length_length; i++) {
+	for(i=0; i<m_queue_length; i++) {
 		if(tmp_front == QUEUE_SIZE) {
 			tmp_front = 0;
 		}
@@ -105,8 +105,21 @@ void main(void)
 		queue_insert(i);
 	}
 	queue_print();
-	printf("[queue] empty: %d, full: %d, length:%d\n", queue_isempty(), queue_isfull(), length_length());
+	printf("[queue] empty: %d, full: %d, length:%d\n", queue_isempty(), queue_isfull(), queue_length());
+	printf("[queue] first: %d\n", queue_first());
+	
+	for(i=0; i<15; i++) {
+		queue_delete();
+	}
+	queue_print();
+	printf("[queue] empty: %d, full: %d, length:%d\n", queue_isempty(), queue_isfull(), queue_length());
+
+	for(i=50; i<65; i++) {
+		queue_insert(i);
+	}
+	queue_print();
+	printf("[queue] empty: %d, full: %d, length:%d\n", queue_isempty(), queue_isfull(), queue_length());
+	printf("[queue] first: %d\n", queue_first());
+
 }
-
-
 
